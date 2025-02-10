b30c264b58248e7b375e519c2670e3ff2d768390
minhnguyen9702 <minhnguyen9702@gmail.com>
2025-02-10 15:10:50 +0700

    add view and route for index

---
+++ app.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+
+const express = require("express");
+const app = express();
+const path = require("node:path");
+const indexRouter = require("./routes/indexRouter");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", indexRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
